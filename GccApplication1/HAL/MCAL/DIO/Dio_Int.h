0639e4a925ab1b580c2c74da68bfc977d8cc9029
mohammedelwardany <mohammed.aael@fci.suezuni.edu.eg>
2023-09-10 00:09:17 +0300

    lb

--- GccApplication1/HAL/MCAL/DIO/Dio_Int.h
+++ GccApplication1/HAL/MCAL/DIO/Dio_Int.h
@@ -17,7 +17,17 @@
 #include "DIO_prv.h"
 
 
+typedef enum {
+	Pin_A0  ,Pin_A1	,Pin_A2	,Pin_A3	,Pin_A4	,Pin_A5	, Pin_A6 ,Pin_A7 ,
+	Pin_B0	,Pin_B1 ,Pin_B2 ,Pin_B3	,Pin_B4 ,Pin_B5	, Pin_B6 ,Pin_B7 ,
+	Pin_C0	,Pin_C1	,Pin_C2	,Pin_C3	,Pin_C4	,Pin_C5	, Pin_C6 ,Pin_C7 ,
+	Pin_D0	,Pin_D1	,Pin_D2	,Pin_D3	,Pin_D4	,Pin_D5	, Pin_D6 ,Pin_D7 ,
+}Pin_Type;
 
+typedef enum {
+	OutPut_High , OutPut_Low,
+	InPut_Float,InPut_PullUp
+}PinMode_type;
 
 
 
