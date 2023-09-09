a7dbb8db98c0d65535e565ca2e56d979c2ad3fae
mohammedelwardany <mohammed.aael@fci.suezuni.edu.eg>
2023-09-10 00:31:43 +0300

    OK

--- GccApplication1/main.c
+++ GccApplication1/main.c
@@ -10,12 +10,12 @@
 int main(void)
 {
 	DIO_Init();
-	Dio_SetPinMode(Pin_A1,OutPut_Low);
+	Dio_SetPinMode(Pin_A1,OutPut_High);
     while (1) 
     {
-		DIO_WritePinVal(Pin_A1,HIGH);
-		_delay_ms(50);
 		DIO_WritePinVal(Pin_A1,LOW);
+		_delay_ms(50);
+		DIO_WritePinVal(Pin_A1,HIGH);
 		_delay_ms(50);
     }
 }
